104621497141665a57a723eb5f7a937bd78dc250
23f1001065 <23f1001065@ds.study.iitm.ac.in>
2024-08-10 10:43:26 +0530

    added python file via upload
    These are the controller file in python

---
+++ tools.py
@@ -0,0 +1,14 @@
+from random import choice
+from string import digits
+from  uuid import uuid4
+from datetime import datetime
+def generate_random_id(user,digit_len = 8):
+    digit_area = ''.join([choice(digits) for _ in range(digit_len)])
+    if user == 'INFE':
+        return user+digit_area
+    if user == "SPON":
+        return user+digit_area
+    if user == "CAMP":
+        return user+digit_area
+    if user == "AD":
+        return user+digit_area
