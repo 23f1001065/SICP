40e18dfe1e18fe53af9252e65213ff5512521ff8
23f1001065 <23f1001065@ds.study.iitm.ac.in>
2024-08-12 20:05:09 +0530

    Update config.py

--- application/config.py
+++ application/config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class ProjectDevelopmentConfig():
     SQLITE_DB_DIR = basedir
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(SQLITE_DB_DIR,'projectDB.db')
-    DEBUG = True
+    DEBUG = False
     SECRET_KEY = token_urlsafe()
     UPLOAD_FOLDER = 'static\\uploads'
