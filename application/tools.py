4b0e20e969328663a0501ba9d8afde7bc3b8a3cd
23f1001065 <23f1001065@ds.study.iitm.ac.in>
2024-12-08 21:29:01 +0530

    Added comment from Ubantu CLI

--- application/tools.py
+++ application/tools.py
@@ -2,6 +2,7 @@ from random import choice
 from string import digits
 from  uuid import uuid4
 from datetime import datetime
+#function for generating user id for uniqely alocate users by id
 def generate_random_id(user,digit_len = 8):
     digit_area = ''.join([choice(digits) for _ in range(digit_len)])
     if user == 'INFE':
